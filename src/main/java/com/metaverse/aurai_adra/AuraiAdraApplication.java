5e724a1477132ca8b2055faff03a65a47aa3cdc6
LeeJoongYeon <ian0209@naver.com>
2025-08-18 11:28:59 +0900

    chore: First init

---
+++ src/main/java/com/metaverse/aurai_adra/AuraiAdraApplication.java
@@ -0,0 +1,13 @@
+package com.metaverse.aurai_adra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuraiAdraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuraiAdraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/metaverse/aurai_adra/AuraiAdraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metaverse.aurai_adra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraiAdraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
